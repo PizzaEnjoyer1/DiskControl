6b3a73e6e0a4f51876c3ac4513afc201c2804cc6
PizzaEnjoyer1 <133069709+PizzaEnjoyer1@users.noreply.github.com>
2024-11-13 20:18:14 -0500

    Update app.py

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ st.image(image, width = 720)
 st.write("Activa algún modo del traje")
 
 # Botón para reconocimiento de voz
-stt_button = Button(label=" Inicio ", width=400)
+stt_button = Button(label=" Presiona y habla ", width=400)
 
 stt_button.js_on_event("button_click", CustomJS(code="""
     var recognition = new webkitSpeechRecognition();
